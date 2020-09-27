c3cde695a45d52afc9a117e2ff0e59d2c7aee3bd
sunweiqi <921939025@qq.com>
2020-09-27 17:46:29 +0800

    子应用路由跳转

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,18 @@
+import {push} from "connected-react-router";
 import React from "react";
+import {useDispatch} from "react-redux";
 
-export default function Profile() {
-  return <div>子应用 我的</div>;
-}
+const Home: React.FC = () => {
+  const dispatch = useDispatch();
+  const goto = () => {
+    dispatch(push("/index"));
+  };
+  return (
+    <div>
+      子应用 我的
+      <button onClick={goto}>index</button>
+    </div>
+  );
+};
+
+export default Home;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import {push} from "connected-react-router";
 import React from "react";
+import {useDispatch} from "react-redux";
 
-export default function index() {
-  return <div>子应用 首页</div>;
-}
+const Index: React.FC = () => {
+  const dispatch = useDispatch();
+  const goto = () => {
+    dispatch(push("/home"));
+  };
+  return (
+    <div>
+      子应用 首页
+      <button onClick={goto}>home</button>
+    </div>
+  );
+};
+
+export default Index;
